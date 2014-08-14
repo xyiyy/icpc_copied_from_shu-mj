5714a2121d6c40be3d376948d4c72ce66688a783
Jun MEI <meijun1993@gmail.com>
2014-08-15 01:07:27 +0800

    First commit.

--- archive/2014.06/2014.06.29 - Single Round Match 626/NegativeGraphDiv1.java
+++
@@ -1,9 +0,0 @@
-package main;
-import java.util.*;
-import com.shu_mj.tpl.Algo;
-
-public class NegativeGraphDiv1 {
-    public long findMin(int N, int[] from, int[] to, int[] weight, int charges) {
-        return 0L;
-    }
-}
--- archive/2014.06/2014.06.29 - Single Round Match 626/ReflectiveRectangle.java
+++
@@ -1,9 +0,0 @@
-package main;
-import java.util.*;
-import com.shu_mj.tpl.Algo;
-
-public class ReflectiveRectangle {
-    public int findSum(int sideA, int sideB, int bounces) {
-        return 0;
-    }
-}
