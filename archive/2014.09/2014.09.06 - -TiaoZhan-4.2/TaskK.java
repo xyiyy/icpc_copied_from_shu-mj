a2b595701af2199a12a030064e6aebd0661e55c8
Jun MEI <jun.mei@outlook.com>
2014-09-07 11:47:36 +0800

    <TiaoZhan> 4.2

--- archive/2014.09/2014.09.06 - -TiaoZhan-4.2/TaskK.java
+++ archive/2014.09/2014.09.06 - -TiaoZhan-4.2/TaskK.java
@@ -1,5 +1,7 @@
 package main;
 
+
+
 import com.shu_mj.tpl.Scanner;
 import java.io.PrintWriter;
 import java.util.*;
@@ -32,23 +34,31 @@ public class TaskK {
 
     private int grundy(int o, int s1, int t1, int s2, int t2) {
         if (dp[o][s1 + 2][t1 + 2][s2 + m + 2][t2 + m + 2] != -1) return dp[o][s1 + 2][t1 + 2][s2 + m + 2][t2 + m + 2];
-        Set<Integer> set = new HashSet<Integer>();
+//        Set<Integer> set = new HashSet<Integer>();
+        BitSet set = new BitSet();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) if ((((i ^ j) & 1) ^ o) == 0 && inS(i + j, s1, t1) && inS(i - j, s2, t2)) {
                 if (maps[i][j] == 'L') {
-                    set.add(grundy(o, s1, i + j - 1, s2, t2) ^ grundy(o, i + j + 1, t1, s2, t2));
+//                    set.add(grundy(o, s1, i + j - 1, s2, t2) ^ grundy(o, i + j + 1, t1, s2, t2));
+                    set.set(grundy(o, s1, i + j - 1, s2, t2) ^ grundy(o, i + j + 1, t1, s2, t2));
                 } else if (maps[i][j] == 'R') {
-                    set.add(grundy(o, s1, t1, s2, i - j - 1) ^ grundy(o, s1, t1, i - j + 1, t2));
+//                    set.add(grundy(o, s1, t1, s2, i - j - 1) ^ grundy(o, s1, t1, i - j + 1, t2));
+                    set.set(grundy(o, s1, t1, s2, i - j - 1) ^ grundy(o, s1, t1, i - j + 1, t2));
                 } else { // X
-                    set.add(grundy(o, s1, i + j - 1, s2, i - j - 1) ^
+//                    set.add(grundy(o, s1, i + j - 1, s2, i - j - 1) ^
+//                            grundy(o, s1, i + j - 1, i - j + 1, t2) ^
+//                            grundy(o, i + j + 1, t1, s2, i - j - 1) ^
+//                            grundy(o, i + j + 1, t1, i - j + 1, t2));
+                    set.set(grundy(o, s1, i + j - 1, s2, i - j - 1) ^
                             grundy(o, s1, i + j - 1, i - j + 1, t2) ^
                             grundy(o, i + j + 1, t1, s2, i - j - 1) ^
                             grundy(o, i + j + 1, t1, i - j + 1, t2));
                 }
             }
         }
-        int res = 0;
-        while (set.contains(res)) res++;
+        int res = set.nextClearBit(0);
+//        int res = 0;
+//        while (set.contains(res)) res++;
         return dp[o][s1 + 2][t1 + 2][s2 + m + 2][t2 + m + 2] = res;
     }
 
