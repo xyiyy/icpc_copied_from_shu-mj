6d77a575201fd1c907e4f6f13a27ecc3db854546
Jun MEI <jun.mei@outlook.com>
2014-10-07 13:23:49 +0800

    Codeforces

--- src/main/Tmp.java
+++ src/main/Tmp.java
@@ -1,11 +1,13 @@
 package main;
 
+import com.shu_mj.ds.KMP;
 import com.shu_mj.ds.Palindrome;
 import com.shu_mj.graph.SCC;
 import com.shu_mj.tpl.Algo;
 import com.shu_mj.tpl.Scanner;
 
 import java.io.*;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
