718c611caf70f6f1b203e99e8ebc0102c6cbb9a6
Jun MEI <jun.mei@outlook.com>
2014-12-03 22:09:40 +0800

    .

--- archive/2014.12/2014.12.03 - USACO/USACO.java
+++ archive/2014.12/2014.12.03 - USACO/USACO.java
@@ -1,17 +1,16 @@
 package main;
 
-import com.shu_mj.ds.Hash;
+import com.shu_mj.ds.UnionFindSet;
+import com.shu_mj.tpl.*;
 import com.shu_mj.tpl.Scanner;
+
 import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.math.MathContext;
 import java.util.*;
-import com.shu_mj.tpl.Algo;
 
 public class USACO {
     /*
     LANG: JAVA
-    TASK: fracdec
+    TASK: agrinet
      */
     Scanner in;
     PrintWriter out;
@@ -24,48 +23,41 @@ public class USACO {
 
     void run() {
         int n = in.nextInt();
-        int d = in.nextInt();
-        String res = (n / d) + "." + calc(n % d, d);
-        for (int i = 0; i < res.length(); i++) {
-            out.print(res.charAt(i));
-            if (i % 76 == 75) out.println();
-        }
-        if (res.length() % 76 != 75) out.println();
-    }
-
-    int L = 1024000;
-    private String calc(int n, int d) {
-        int[] is = new int[L];
-        for (int i = 0; i < L; i++) {
-            n *= 10;
-            is[i] = n / d;
-            n %= d;
-            if (n == 0) {
-                StringBuilder sb = new StringBuilder(i + 1);
-                for (int j = 0; j <= i; j++) sb.append(is[j]);
-                return sb.toString();
-            }
-        }
-        Hash H = new Hash(L);
-        long[] hs = H.build(is);
-        for (int i = 0; ; i++) {
-            for (int j = 1; j < L / 3; j++) {
-                if (fit(H, hs, i, j)) {
-                    StringBuilder sb = new StringBuilder(i + j + 10);
-                    for (int k = 0; k < i; k++) sb.append(is[k]);
-                    sb.append('(');
-                    for (int k = i; k < i + j; k++) sb.append(is[k]);
-                    sb.append(')');
-                    return sb.toString();
+        P[] ps = new P[n * (n - 1) / 2];
+        for (int i = 0, c = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int d = in.nextInt();
+                if (i < j) {
+                    ps[c++] = new P(d, i, j);
                 }
             }
         }
+        Arrays.sort(ps);
+        UnionFindSet uf = new UnionFindSet(n);
+        int sum = 0;
+        for (P p : ps) {
+            if (!uf.isSame(p.u, p.v)) {
+                uf.union(p.u, p.v);
+                sum += p.cost;
+            }
+        }
+        out.println(sum);
     }
 
-    private boolean fit(Hash H, long[] hs, int b, int l) {
-        for (int i = b; i + l < L - 10; i += l) {
-            if (H.get(hs, b, b + l) != H.get(hs, i, i + l)) return false;
+    class P implements Comparable<P> {
+        int cost;
+        int u;
+        int v;
+
+        P(int cost, int u, int v) {
+            this.cost = cost;
+            this.u = u;
+            this.v = v;
         }
-        return true;
+
+        @Override
+        public int compareTo(P o) {
+            return cost - o.cost;
+        }
     }
 }
